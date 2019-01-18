dc501326ca59ed5288df0ffddda2481309b5bcaf
I0101010101010 <matyushkin.roma@gmail.com>
2019-01-18 14:22:43 +0500

    Add project files.

---
+++ ApiOutdatedFinding/Notes/Readme.txt
@@ -0,0 +1,41 @@
+﻿--------------------------Task---------------------------------------------
+
+Write a program to find out app which are running under old version.
+
+--------------------------Sample----------------------------------------------
+
+Input file: 
+
+Mail App, Authentication API, v6
+Video Call App, Authentication API, v7
+Mail App, Data Storage API, v10
+Chat App, Data Storage API, v11
+Mail App, Search API, v6
+Chat App, Authentication API, v8
+Chat App, Presence API, v2
+Video Call App, Data Storage API, v11
+Video Call App, Video Compression API, v3
+
+
+Output file: 
+ 
+Mail App
+Video Call App
+
+------------------------------Solution--------------------------------------
+
+To resolve the problem we need grouping by API name. Sort in each group by API version and select all which version is not max:
+
+Mail App, Authentication API, v6
+Video Call App, Authentication API, v7
+Chat App, Authentication API, v8
+
+Mail App, Data Storage API, v10
+Video Call App, Data Storage API, v11
+Chat App, Data Storage API, v11
+
+Mail App, Search API, v6
+
+Video Call App, Video Compression API, v3
+
+Chat App, Presence API, v2
---
+++ ApiOutdatedFinding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiOutdatedFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Preparation
+            string[] lines = File.ReadAllLines("Files/input.txt");
+            List<(string ApplicationName, string ApiName, int ApiVersion)> linesPrepared = new List<(string ApplicationName, string ApiName, int ApiVersion)>();
+            lines.ToList().ForEach(x =>
+            {
+                string[] splited = x.Split(',');
+                linesPrepared.Add((splited[0], splited[1], Convert.ToInt32(splited[2].Trim().Replace("v", string.Empty))));
+            });
+            List<string> oldestApps = new List<string>();
+
+
+            //Action
+            linesPrepared.GroupBy(x => x.ApiName).Where(y => y.Count() > 1).ToList().ForEach(g =>
+            {
+                var oldests = g.Where(x => x.ApiVersion != g.Max(y => y.ApiVersion));
+                oldestApps.AddRange(oldests.Select(x => x.ApplicationName));
+            });
+
+            File.WriteAllLines("Files/output.txt", oldestApps.Distinct());
+        }
+    }
+}
